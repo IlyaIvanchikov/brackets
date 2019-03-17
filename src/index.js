99f29dba264fda7efe1ba807dc19c6f61319476b
IlyaIvanchikov <phantomonster1@mail.ru>
2019-03-17 12:55:24 +0200

    feat: add my answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var arr = str.split ('')
+  var arr1 = bracketsConfig.flat(1);
+  var itog = 0;
+  if (str.length % 2 != 0) {
+      itog = false;}
+  else if (str.length % 2 == 0) {
+      for ( var a = 0; a < arr.length; a++) {
+          for ( var b = 0; b < arr1.length; b++) {
+              if (arr[a] == arr1[b]) {
+                  if ((arr[a] == arr1[b]) && (arr[a+1] == arr1[b+1])) {
+                      arr.splice(a, 2); 
+                      a = 0;
+                      b = -1;
+                  }  
+              } 
+          }
+      } 
+      if (arr.length == 0) {
+          itog = true;
+      }     
+  }
+  return  itog;
+  }
+
